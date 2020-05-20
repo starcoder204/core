675a568a1dce825e8dc91d4d769f2a09db3f20d6
dianstarcoder <dian@dev.cybens.com>
2020-05-20 16:45:55 +0800

    remove unnecessary files

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$("#submit").click(function(){
-		if($("password").val() != $("confirm_password").val()){
-			alert("Enter password correctly!");
-		}else{
-			$("register-from").submit();
-		}
-	});
-});
